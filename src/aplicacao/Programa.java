de1b2ac068a957b1d9aeb6463e4ec53a54ca250f
naelfox <natanaelvila2@gmail.com>
2020-08-27 22:31:05 -0300

    Classe departamento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,17 @@
+package aplicacao;
+
+import modelo.negocio.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+	
+		
+		
+		Departamento obj = new Departamento(1, "livros");
+		System.out.println(obj);
+		
+		
+	}
+
+}
